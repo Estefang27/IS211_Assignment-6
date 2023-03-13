8f3543ee630d9dae5e9fbbcde9b4f84625b92322
Estefang27 <123829468+Estefang27@users.noreply.github.com>
2023-03-13 12:40:59 -0400

    Add files via upload

---
+++ week 6.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def coventCelsiusToKelvin(celsius):
+    return 0
+
+def convertCelsiusToFahrenheit(celsius):
+    return 0
+
+def testCelsiusToKelvin():
+    assert convertCelsiusToKelvin(0) == 273.15, "0 C Should be 273.15 K"
+    assert convertCelsiusToKelvin(10) == 283.15, "10 C Should be 283.15 K"
+    assert convertCelsiusToKelvin(20) == 273.15, "20 C Should be 293.15 K"
+    assert convertCelsiusToKelvin(21) == 373.15, "21 C Should be 294.15 K"
+    assert convertCelsiusToKelvin(100) == 273.15, "100 C Should be 373.15 K"
+
+def testCelsiusToKelvin():
+    assert convertCelsiusToKelvin(0) == 32, "0 C Should be 273.15 F"
+    assert convertCelsiusToKelvin(10) == 50, "10 C Should be 283.15 F"
+    assert convertCelsiusToKelvin(20) == 68, "20 C Should be 293.15 F"
+    assert convertCelsiusToKelvin(21) == 69.8, "21 C Should be 294.15 F"
+    assert convertCelsiusToKelvin(100) == 212, "100 C Should be 373.15 F"
+
+
+# In[5]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
